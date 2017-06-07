99bcd281432b0616e5822e7e949d5dec0a8e4204
Arpad <arpafaucon@gmail.com>
2017-06-07 09:44:40 +0000

    prez

--- src/amiralTitanic.cpp
+++ src/amiralTitanic.cpp
@@ -12,18 +12,21 @@
 #include <nav_msgs/OccupancyGrid.h>
 
 #define ROBOT_WIDTH 0.40
-#define SAFE_DISTANCE 1.
+#define SAFE_DISTANCE 0.5
 
 qg::ordreAmiral* msg;
 
 nav_msgs::OccupancyGrid grid;
 nav_msgs::MapMetaData meta;
 
+
 ros::Publisher chatter_pub;
 ros::Subscriber rtab_listener;
 double robot_x, robot_y, robot_yaw, hz_dx, hz_dy, vt_dx, vt_dy;
 double resolution, grid_x, grid_y;
 int grid_height, grid_width;
+bool valid_grid;
+bool needDisplay;
 
 int getCellIndex(double x, double y);
 void gridListener(const nav_msgs::OccupancyGrid& msg);
@@ -40,7 +43,11 @@ void gridListener(const nav_msgs::OccupancyGrid& msg)
 	resolution = meta.resolution;
 	grid_height = meta.height;
 	grid_width = meta.width;
-	ROS_INFO("Updated map . Now %d %d", grid_height, grid_width);
+	//~ ROS_INFO("Updated map . Now %d %d", grid_height, grid_width);
+	//~ std::cout << getCellIndex(grid_x , grid_y) << ":" << getCellIndex(grid_x + resolution, grid_y) << ":" << getCellIndex(grid_x, grid_y + resolution) <<std::endl;
+	//~ std::cout << getCellIndex(grid_x + resolution, grid_y + resolution) << ":" << getCellIndex(grid_x + 2*resolution, grid_y) << ":" << getCellIndex(grid_x, grid_y + 2*resolution) <<std::endl;
+	valid_grid = true;
+	needDisplay = true;
 }
 
 void ordreSimple(float motor, float direction, float temps){
@@ -56,25 +63,142 @@ void ordreSimple(float motor, float direction, float temps){
 
 }
 
+void printGrid(int8_t pgrid[]){
+	if(needDisplay){
+		std::cout<< "Printing Grid "<< grid_height << "x" << grid_width << "  " << grid_x << ";" << grid_y << "/" << resolution << std::endl ;
+		int8_t val;
+		for(int j = 0; j < grid_height ; j++){
+			for(int i = 0 ; i < grid_width ; i++){
+				val = pgrid[ i + (grid_height - j -1)* grid_width];
+				if(val == 100){
+					std::cout<<".";
+				} else if(val == 101){
+					std::cout<<"R";
+				} else if(val == 102){
+					std::cout<<"X";
+				} else{
+					std::cout<<" ";
+				}
+			}
+			std::cout<<"|"<<std::endl;
+			needDisplay = false;
+		}
+	}
+}
+
+void printBothGrid(int8_t pgrid[]){
+	if(needDisplay){
+		std::cout<< "Printing Grid "<< grid_height << "x" << grid_width << "  " << grid_x << ";" << grid_y << "/" << resolution << std::endl ;
+		int8_t val;
+		for(int j = 0; j < grid_height ; j++){
+			for(int i = 0 ; i < grid_width ; i++){
+				val = pgrid[ i + (grid_height - j -1)* grid_width];
+				if(val == 100){
+					std::cout<<".";
+				} else if(val == 101){
+					std::cout<<"R";
+				} else if(val == 102){
+					std::cout<<"X";
+				} else{
+					std::cout<<" ";
+				}
+			}
+			std::cout<<" | ";
+			for(int i = 0 ; i < grid_width ; i++){
+				val = grid.data[ i + (grid_height - j -1)* grid_width];
+				if(val == 100){
+					std::cout<<".";
+				} else if(val == 101){
+					std::cout<<"R";
+				} else if(val == 102){
+					std::cout<<"X";
+				} else if(val == 0){
+					std::cout<<"0";
+				} else{
+					std::cout<<" ";
+				}
+			}
+			std::cout << "|" << std::endl
+		}
+		needDisplay = false;
+		
+	}
+}
+
+void printOccupancyGrid(){
+	if(needDisplay){
+		std::cout<< "Printing OccupancyGrid "<< std::endl;
+		int8_t val;
+		for(int j = 0; j < grid_height ; j++){
+			for(int i = 0 ; i < grid_width ; i++){
+				val = grid.data[ i + (grid_height - j -1)* grid_width];
+				if(val == 100){
+					std::cout<<".";
+				} else if(val == 101){
+					std::cout<<"R";
+				} else if(val == 102){
+					std::cout<<"X";
+				} else if(val == 0){
+					std::cout<<"0";
+				} else{
+					std::cout<<" ";
+				}
+			}
+			std::cout<<"|"<<std::endl;
+		}
+	}
+}
+
 bool testPath(){
+	//~ ROS_INFO("testing path");
 	int w = (int) ceil(ROBOT_WIDTH / resolution / 2);
 	int h = (int) ceil(SAFE_DISTANCE / resolution );
-	int value, index;
+	int index;
+	int8_t value;
+	bool pathClear = true;
+	
+	if(!valid_grid){
+		return true;
+	}
+	int8_t requestedCells[grid_height*grid_width] = {0};
+	//~ printOccupancyGrid();
+	//pirnt position of robot
+	index = getCellIndex(robot_x, robot_y);
+	//~ if(index != -1){
+		//~ ROS_INFO("index of robot %d", index);
+		//~ requestedCells[index] = 101;
+	//~ } else {
+		//~ ROS_INFO("robot not found ? R : %lf %lf G : %lf %lf  res : %lf", robot_x, robot_y, grid_x, grid_y, resolution);
+	//~ }
 	for(int i = -w; i <= w ; i++){
 		// width of robot
 		for(int j = 0; j < h ; j++){
 			index = getCellIndex(robot_x + i * hz_dx + j * vt_dx, robot_y + i * hz_dy + j * vt_dy);
 			if(index != -1){
+				//~ ROS_INFO("lindex %d", index);
 				value =  grid.data[index];
-				if(value == 100){
+				requestedCells[index] = 100;
+				//~ ROS_INFO("value %d", value);
+				if(value > 60){
 					//occupied cell
-					ROS_INFO("Found an occupied cell in coordinates %d;%d. Stopping.", i, j);
-					return false;
+					requestedCells[index] = 102;
+					//~ printGrid(requestedCells);
+					//~ ROS_INFO("Found an occupied cell in coordinates %d;%d. Stopping.", i, j);
+					pathClear = false;
+					
 				}
 			}
 		}
 	}
-	return true;
+	index = getCellIndex(robot_x, robot_y);
+	if(index != -1){
+		//~ ROS_INFO("index of robot %d", index);
+		requestedCells[index] = 101;
+	} else {
+		ROS_INFO("robot not found ? R : %lf %lf G : %lf %lf  res : %lf", robot_x, robot_y, grid_x, grid_y, resolution);
+	}
+	printBothGrid(requestedCells);
+	return pathClear;
 }
 
 void generateDx(){
@@ -87,11 +211,13 @@ void generateDx(){
 
 int getCellIndex(double x, double y){
 	//checkBounds
-	if( x < grid_x || x > grid_x + grid_width * resolution || y < grid_y || y > grid_y + grid_height * resolution){
+	if( x < grid_x  || x > grid_x + grid_width * resolution || y < grid_y || y > grid_y + grid_height * resolution){
 		//out of bounds
+		
 		return -1;
 	}
-	double dx = x - robot_x, dy = y - robot_y;
+	//double dx = x - robot_x, dy = y - robot_y;
+	double dx = x - grid_x, dy = y - grid_y;
 	int ix = (int) floor(dx / resolution );
 	int iy = (int) floor(dy / resolution );
 	int index = iy * grid_width + ix;
@@ -143,6 +269,7 @@ int main(int argc, char** argv){
 	
 	
 	
+	
 	while(ros::ok()){
 		//recuperation de la localistation du robot
 		tf::StampedTransform transform;
@@ -164,7 +291,7 @@ int main(int argc, char** argv){
     robot_yaw = tf::getYaw(transform.getRotation());
     generateDx();
 
-		ROS_INFO("%lf, %lf, %lf", robot_x, robot_y, robot_yaw);
+		//~ ROS_INFO("%lf, %lf, %lf", robot_x, robot_y, robot_yaw);
 
 		//decision
 		if(testPath()){
@@ -173,15 +300,16 @@ int main(int argc, char** argv){
 		ordreSimple(0.0,0.0,0.5);
 		}
 		
-		ROS_INFO("launched orders");
+		//~ ROS_INFO("launched orders");
 		ros::spinOnce();
 		loop_rate.sleep();
 		
 		if(ros::isShuttingDown()){
+			ROS_INFO("Detected shutdown request");
 			ordreSimple(0.0,0.0,1);
 		}
 	}
-  
+  ROS_INFO("Detected shutdown request");
   //terminaison
 	ordreSimple(0.0,0.0,1);
 	ros::shutdown();
